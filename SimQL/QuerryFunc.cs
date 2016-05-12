c81f8fe0855615de1d2398fd52a754ba66b5e9fe
Arkady.Anderjushkin <kanaroooo@gmail.com>
2016-05-12 16:59:16 +0600

    QueryFunc

--- SimQL/QuerryFunc.cs
+++ SimQL/QuerryFunc.cs
@@ -1,14 +1,41 @@
+using System;
+using System.Globalization;
+
 namespace SimQLTask
 {
-	public struct QuerryFunc
+	public struct QueryFunc
 	{
 		public readonly string Name;
-		public readonly string Args;
+		public readonly string Selector;
+		public readonly double? From;
+		public readonly int? Top;
 
-		public QuerryFunc(string name, string args)
+		public QueryFunc(string name, string args, int? top = null, double? @from = null)
 		{
 			Name = name;
-			Args = args;
+			Selector = args;
+			Top = top;
+			From = @from;
+		}
+
+		public static QueryFunc Parse(string s)
+		{
+			var seps = new[]
+						{
+							'(', ')', ' '
+						};
+			var parsed = s.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+			switch (parsed.Length)
+			{
+				case 6:
+					return new QueryFunc(parsed[0].ToLower(), parsed[1], int.Parse(parsed[5], NumberStyles.Any, CultureInfo.InvariantCulture), double.Parse(parsed[3], NumberStyles.Any, CultureInfo.InvariantCulture));
+				case 4:
+					return new QueryFunc(parsed[0].ToLower(), parsed[1], int.Parse(parsed[5], NumberStyles.Any, CultureInfo.InvariantCulture));
+				default:
+					return new QueryFunc(parsed[0].ToLower(), parsed[1]);
+
+			}
+
 		}
 	}
 }
--- SimQL/QuerryFunctions.cs
+++ SimQL/QuerryFunctions.cs
@@ -2,11 +2,7 @@ namespace SimQLTask
 {
 	public static class QuerryFunctions
 	{
-		public static QuerryFunc Parse(string s)
-		{
-			var parsed = s.Split('(', ')');
-			return new QuerryFunc(parsed[0].ToLower(), parsed[1]);
-		}
+
 
 	}
 }
