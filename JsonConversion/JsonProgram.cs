81c2ac7e4b89d6c0871fe6d33b48cdd471112c0a
uc-user <uc-user>
2016-05-12 16:11:09 +0500

    Json3 fix

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
@@ -11,7 +14,8 @@ namespace JsonConversion
 		{
 			string json = Console.In.ReadToEnd();
 //			JObject v2 = JObject.Parse(json);
-
+//		    Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+		    Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator = ",";
             var wareHouseV2 = ParseToWareHouseV2(json);
 		    var wareHouseV3 = WareHouseConverter.Convert(wareHouseV2);
 		    var result = JsonConvert.SerializeObject(wareHouseV3);
@@ -22,25 +26,43 @@ namespace JsonConversion
 
 	    public static WareHouseV2 ParseToWareHouseV2(string json)
 	    {
-	        return JsonConvert.DeserializeObject<WareHouseV2>(json.Replace("'", ""));
+            return JsonConvert.DeserializeObject<WareHouseV2>(CorrectInputJson(json));
 	    }
 
-//        public class JsonDoubleConverter : JsonConverter
-//        {
-//            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-//            {
-//                throw new NotImplementedException();
-//            }
-//
-//            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-//            {
-//                throw new NotImplementedException();
-//            }
-//
-//            public override bool CanConvert(Type objectType)
-//            {
-//                return 
-//            }
-//        }
+	    private static string CorrectInputJson(string json)
+	    {
+	        var replace = json.Replace("'", "");
+	        var result = replace;
+            var regex = new Regex(@",\d\d""");
+            var matches = regex.Matches(replace);
+	        foreach (Match match in matches)
+	        {
+	            result = result.Remove(match.Index, 1).Insert(match.Index, ".");
+	        }
+	        return result;
+	    }
 	}
+
+    internal class FormattedDecimalConverter : JsonConverter
+    {
+        public static double ParseDouble(string input)
+        {
+            var value = input.Replace("'", "");
+            return double.Parse(value, CultureInfo.InvariantCulture);
+        }
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return ParseDouble((string) reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(double) == objectType;
+        }
+    }
 }
